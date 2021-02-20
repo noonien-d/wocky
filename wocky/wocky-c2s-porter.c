cdb05d961e260fdfe3334553f36e48c7239c4e37
Ferdinand Stehle <development@kondorgulasch.de>
2021-02-20 15:21:30 +0100

    use portable integer format specifiers

--- wocky/wocky-c2s-porter.c
+++ wocky/wocky-c2s-porter.c
@@ -1476,7 +1476,7 @@ resume_connection (WockyC2SPorter *self)
   WockyC2SPorterPrivate *priv = wocky_c2s_porter_get_instance_private (self);
   WockyStanza *resume = wocky_stanza_new ("resume", WOCKY_XMPP_NS_SM3);
   WockyNode *rn = wocky_stanza_get_top_node (resume);
-  gchar *val = g_strdup_printf ("%lu", priv->sm->rcv_count);
+  gchar *val = g_strdup_printf ("%zu", priv->sm->rcv_count);
   gboolean ret = TRUE;
 
   g_assert (priv->sm);
@@ -1624,7 +1624,7 @@ wocky_porter_sm_handle_h (WockyC2SPorter *self,
                       ':', WOCKY_XMPP_NS_SM3, ')',
                     NULL);
 
-      DEBUG ("Invalid acknowledgement %lu, must be between %lu and %lu",
+      DEBUG ("Invalid acknowledgement %zu, must be between %zu and %zu",
           snt, priv->sm->snt_acked, priv->sm->snt_count);
       wocky_porter_send_async (WOCKY_PORTER (self), error,
           priv->receive_cancellable, wocky_porter_sm_error_cb, self);
@@ -1633,7 +1633,7 @@ wocky_porter_sm_handle_h (WockyC2SPorter *self,
       return FALSE;
     }
 
-  DEBUG ("Acking %lu stanzas handled by the server", snt);
+  DEBUG ("Acking %zu stanzas handled by the server", snt);
   priv->sm->snt_acked = snt;
   /* now we can head-drop stanzas from unacked_queue till its length is
    * (snt_count - snt_acked) */
@@ -1644,7 +1644,7 @@ wocky_porter_sm_handle_h (WockyC2SPorter *self,
       g_assert (s);
       g_object_unref (s);
     }
-  DEBUG ("After ack %u(%lu) stanzas left in the queue",
+  DEBUG ("After ack %u(%zu) stanzas left in the queue",
       g_queue_get_length (priv->unacked_queue),
       ACK_WINDOW (priv->sm->snt_acked, priv->sm->snt_count));
   return TRUE;
@@ -1683,7 +1683,7 @@ wocky_porter_sm_handle (WockyC2SPorter *self,
     {
       WockyStanza *a = wocky_stanza_new ("a", WOCKY_XMPP_NS_SM3);
       WockyNode *an = wocky_stanza_get_top_node (a);
-      gchar *val = g_strdup_printf ("%lu", priv->sm->rcv_count);
+      gchar *val = g_strdup_printf ("%zu", priv->sm->rcv_count);
 
       wocky_node_set_attribute (an, "h", val);
       g_free (val);
